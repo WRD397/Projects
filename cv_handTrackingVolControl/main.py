e8dc908c0bdd902c477fd813a47eb38b515bb957
WRD397 <writd97@gmail.com>
2024-05-05 13:16:08 +0530

    added necessary comments.

--- cv_handTrackingVolControl/main.py
+++ cv_handTrackingVolControl/main.py
@@ -6,8 +6,10 @@ import time
 
 def main() -> None:
     
+    ### change the detection confidence to a higher value to make sure, while controlling the volume it doesnt flicker too much
     detectorObj = handDetector()
-    detectorObj.showImage(fps=False)
+    detectorObj.showImage(fps=True, volControlSwitch=True)
+
 
 if __name__=='__main__':
     main()
